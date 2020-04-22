20ac19f18d5f12819cc94d6931c822f619da9b43
Vishnu Mohanan <44225503+Iam-VM@users.noreply.github.com>
2020-04-23 00:38:40 +0530

    Update database.js

--- code gig/config/database.js
+++ code gig/config/database.js
@@ -5,7 +5,7 @@ const {Sequelize} = require("sequelize");
 //The below statements is the instantiation and initialization of the Sequelize class.
 //It is available in sequelize official doc [Getting Started section].
 
-const sequelize = new Sequelize('codegig', 'postgres', '12345678', {
+const sequelize = new Sequelize('codegig', 'whatever the username is', 'whatever the password is', {
   host: 'localhost',
   dialect: 'postgres'
 });
